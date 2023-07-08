b00990e7a1aa45c47563d1540ccafffdc03ca2f0
V-227 <111197517+V-227@users.noreply.github.com>
2023-07-08 21:19:51 +0000

    Testes para o método shuffle e finalização dele

--- Test.java
+++ Test.java
@@ -1,14 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
   
   public static String shuffle(String str) {
+
+   String temporary = "";
+   List<String> words = new ArrayList<>();
     
     for (int i = 0; i < str.length(); i++) {
+      char character = str.charAt(i);
+      
+      if (character != ' ') {
+        
+        temporary = temporary + character;
+        
+      } else {
+        int j = 0; j++;
+        words.add(temporary);
+        temporary = "";
+        
+      }
+    }
+     String newString = "";
+    for (int i = words.size() - 1; i >= 0; i--) {
       
     }
-  
-    
-    
-  
-    return str;
+    return null;
   }
 }
